5ef4346ef7016fb16381442d710d09aff65cbf56
git-richardwu <richardwu720@gmail.com>
2025-03-25 21:49:53 -0400

    Update Tile.tsx

--- client/src/components/Tile.tsx
+++ client/src/components/Tile.tsx
@@ -1,33 +1,46 @@
 import React from "react";
 import { Draggable } from "@hello-pangea/dnd";
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale.css';
+import Tippy from "@tippyjs/react";
 
 interface tileProps {
-    // idx: number;
     unique: string;
     text: string;
+    desc: string;
+    highlight: string;
     index: number;
 }
 
-const Tile: React.FC<tileProps> = ({ unique, text, index }) => {
+const Tile: React.FC<tileProps> = ({ unique, text, desc, highlight, index }) => {
 
     return (
         <Draggable draggableId={unique} index={index}>
             {provided => (
-                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} style={{
-                    ...provided.draggableProps.style, width: '100px',
-                    height: '100px',
-                    // display: 'inline-block',
-                    margin: '0 8px',
-                    backgroundColor: 'lightgrey',
-                    border: '1px solid black',
-                    borderRadius: '4px',
-                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-                    cursor: 'grab',
-                    zIndex: 3
-                }}>
+                <Tippy content={<span>
                     {text}
-                    
-                </div>
+                    <hr />
+                    <span style={{ fontStyle: "oblique" }}>{desc}</span>
+                </span>}>
+                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
+                        style={{
+                            ...provided.draggableProps.style, width: '110px',
+                            height: '110px',
+                            display: 'flex',
+                            margin: '0 4px',
+                            backgroundColor: '#FBEEE0',
+                            textAlign: 'center',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            borderRadius: '4px',
+                            boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+                            cursor: 'grab',
+                            zIndex: 3
+                        }}>
+                        {text}
+
+                    </div>
+                </Tippy>
             )}
         </Draggable>
     );
