448d6782b9391c99fe30a9f3c1784e5aafcbf72d
git-richardwu <richardwu720@gmail.com>
2025-03-25 21:54:47 -0400

    Update Game.tsx

--- client/src/Game.tsx
+++ client/src/Game.tsx
@@ -3,37 +3,49 @@ import { useSocket } from './context';
 import { useParams } from 'react-router-dom';
 import BuildPage from './components/BuildPage';
 import CombatPage from './components/CombatPage';
+import GameOver from './components/GameOver';
 import CalculationPage from './components/CalculationPage';
 import { useNavigate } from 'react-router-dom';
-import { evaluate, parse, random, randomInt } from 'mathjs';
+import PopUp from './components/PopUp';
+import PreventRefresh from './components/PreventRefresh';
+import { motion, AnimatePresence } from "motion/react"
 
-type GameStates = 'build' | 'assignment' | 'calculate';
+
+
+type GameStates = 'build' | 'assignment' | 'calculate' | 'victory' | 'defeat';
 
 interface ItemProps {
-    itemID: string,
-    name: string,
-    icon: string,
-    description: string,
-    rarity: string,
-    cost: number
+    itemID: string;
+    name: string;
+    icon: string;
+    description: string;
+    rarity: string;
+    cost: number;
 }
 
+interface ReceiptProps {
+    before: number;
+    buff: ItemProps;
+    after: number;
+}
 
 export interface PlayerAssignments {
-    variableLetter: string,
-    numberValue: number
+    variableLetter: string;
+    numberValue: number;
 }
 
 export interface PlayerStats {
-    id: string,
-    equation: string,
-    answer: number,
-    assignments: PlayerAssignments,
-    sabotaged: PlayerAssignments,
-    preview: string,
-    final: string,
-    status: boolean,
-    result: string
+    id: string;
+    health: number;
+    equation: string;
+    buffedEquation: string;
+    answer: number;
+    assignments: PlayerAssignments;
+    sabotaged: PlayerAssignments;
+    presabotage: string;
+    final: string;
+    status: boolean;
+    result: string;
 }
 
 export default function Game() {
@@ -45,80 +57,186 @@ export default function Game() {
     const [playerProp, setPlayerProp] = useState<PlayerStats[]>([]);
     const [currentBuild, setCurrentBuild] = useState<ItemProps[]>([]);
     const [currentInventory, setCurrentInventory] = useState<ItemProps[]>([]);
+    const [buffedEquation, setBuffedEquation] = useState<string>("")
+    const [receiptData, setReceiptData] = useState<ReceiptProps[]>([])
     const [currentRelics, setCurrentRelics] = useState<ItemProps[]>([]);
     const [currentCoins, setCurrentCoins] = useState<number>(0);
+    const [currentHealth, setCurrentHealth] = useState<number>(0);
     const [dice, setDice] = useState<number[]>([]);
     const [opponentVariables, setOpponentVariables] = useState<string[]>([]);
     const [equationData, setEquationData] = useState<string>("$")
     const [loading, setLoading] = useState<boolean>(true);
+    const [popUpDetails, setPopUpDetails] = useState<string | null>(null);
+    const [showWarning, setShowWarning] = useState<boolean>(false);
+    const [currentLevel, setCurrentLevel] = useState<number>(1);
+    const [currentProgress, setCurrentProgress] = useState<number>(0);
+    const [currentCap, setCurrentCap] = useState<number>(12);
+    const [iconReceipt, setIconReceipt] = useState<ItemProps[]>([]);
+    const [opponentIconReceipt, setOpponentIconReceipt] = useState<ItemProps[]>([]);
+    const [opPrevEquation, setOpPrevEquation] = useState<string>('2a+b')
+
+
+
+    function removeDupes(arr: ItemProps[]) {
+        const temp = new Set();
+        return arr.filter(obj => {
+            if (!temp.has(obj.name)) {
+                temp.add(obj.name);
+                return true;
+            }
+            return false;
+        });
+    }
 
     useEffect(() => {
-        if (roomId != undefined) {
-            socket.emit('fetchBuild', roomId);
+        if (socket) {
+            if (roomId !== undefined) {
+                socket.emit('fetchBuild', roomId); //catch something here
+            }
+            socket.on('updateState', (deconstructedEq: ItemProps[], playerInventory: ItemProps[], playerItems: ItemProps[], playerCoins: number, playerHealth: number, playerExp: number) => {
+                setCurrentBuild(deconstructedEq);
+                setCurrentInventory(playerInventory);
+                setCurrentRelics(playerItems);
+                setCurrentCoins(playerCoins);
+                setCurrentHealth(playerHealth);
+                setCurrentProgress(playerExp);
+                setLoading(false);
+            })
+            socket.on('updateProgress', (newProgress: number, newCap: number, newLevel: number) => {
+                setCurrentProgress(newProgress);
+                setCurrentCap(newCap);
+                setCurrentLevel(newLevel);
+            })
+            socket.on('clearData', () => {
+                setOpPrevEquation('2a+b');
+                setIconReceipt([]);
+                setOpponentIconReceipt([]);
+            })
+            return () => {
+                socket.off('fetchBuild');
+                socket.off('updateState');
+                socket.off('updateProgress');
+                socket.off('clearData');
+            };
         }
-        socket.on('updateState', (deconstructedEq: ItemProps[], playerInventory: ItemProps[], playerItems: ItemProps[], playerCoins: number) => {
-            setCurrentBuild(deconstructedEq);
-            setCurrentInventory(playerInventory);
-            setCurrentRelics(playerItems);
-            setCurrentCoins(playerCoins);
-            setLoading(false);
-        })
+        else {
+            console.log('no socket');
+        }
+    }, [socket, roomId])
+
+    useEffect(() => {
+        if (socket) {
+            socket.on('sendAssignData', (dies: number[], buffed: string, receipt: ReceiptProps[]) => {
+                setDice(dies);
+                setBuffedEquation(buffed);
+                setReceiptData(receipt);
+                const allInteractions = receipt.map(interaction => interaction.buff)
+                const uniqueInteractions = removeDupes(allInteractions)
+                setIconReceipt(uniqueInteractions);
+            })
+            socket.on('opponentVars', (variables: string[]) => {
+                setOpponentVariables(variables)
+            })
+            socket.on('lastSeen', (opReceipt: ReceiptProps[], opEquation: string) => {
+                const allOpInteractions = opReceipt.map(interaction => interaction.buff);
+                const uniqueOpInteractions = removeDupes(allOpInteractions);
+                setOpponentIconReceipt(uniqueOpInteractions);
+                setOpPrevEquation(opEquation)
+            })
+            socket.on('roundResults', (preview: PlayerStats[]) => {
+                setPlayerProp(preview);
+                //setYourAssignments(assign);
+            })
+            socket.on('initiateBuild', () => {
+                setGameState('build');
+            })
+            socket.on('initiateCombat', () => {
+                setGameState('assignment');
+            });
+            socket.on('initiateCalculations', () => {
+                socket.emit('fetchBuild', roomId);
+                setGameState('calculate');
+            })
+            socket.on('createErrorMsg', (msg: string) => {
+                setPopUpDetails(msg);
+            });
+            socket.on('opponentDisconnect', (msg: string) => {
+                setPopUpDetails(msg);
+                const timer = setTimeout(() => {
+                    navigate('/');
+                }, 4000)
+                return () => clearTimeout(timer);
+            })
+            return () => {
+                socket.off('sendAssignData');
+                socket.off('initiateCombat');
+                socket.off('opponentVars');
+                socket.off('lastSeen');
+                socket.off('roundResults');
+                socket.off('initiateBuild');
+                socket.off('initiateCalculations');
+                socket.off('createErrorMsg');
+                socket.off('opponentDisconnect');
+            }
+        }
+    }, [navigate, socket, loading, gameState, roomId]);
+
+    useEffect(() => {
+        const handleBeforeUnloadEvent = (event: BeforeUnloadEvent) => {
+            event.preventDefault();
+            setShowWarning(true);
+        }
+        window.addEventListener("beforeunload", handleBeforeUnloadEvent);
         return () => {
-            socket.off('fetchBuild');
-        };
+            window.removeEventListener("beforeunload", handleBeforeUnloadEvent);
+        }
     }, [])
 
-    useEffect(() => {
-        socket.on('pickDiceRolls', (dies: number[]) => {
-            setDice(dies);
-        })
-        socket.on('opponentVars', (variables: string[]) => {
-            setOpponentVariables(variables)
-            //console.log(variables);
-        })
-        socket.on('roundResults', (preview: PlayerStats[]) => {
-            //console.log(preview)
-            setPlayerProp(preview);
-            //setYourAssignments(assign);
-        })
-        socket.on('initiateCombat', () => {
-            setGameState('assignment');
-        });
-        socket.on('initiateCalculations', () => {
-            socket.emit('fetchBuild', roomId);
-            setGameState('calculate');
-        })
-        // socket.on('disconnect', () => {
-        //     console.log('here')
-        //     setSocketStatus(true);
-        // })
-        // if (socketStatus) {
-        //     navigate('/');
-        // }
-        return () => {
-            socket.off('pickDiceRolls');
-            socket.off('initiateCombat')
-            //socket.disconnect();
-        }
-    }, [navigate, socket])
-
-    function handleDataFromBuildPage(submittedEquation: string) { //should probably pass the whole object (InventoryData) in the future
-        setEquationData(submittedEquation)
-        socket.emit('playerReady', roomId, submittedEquation);
+    function handleDataFromBuildPage(submittedEquation: string, deconstructedEquation: ItemProps[], submittedRelics: ItemProps[]) { //should probably pass the whole object (InventoryData) in the future
+        setEquationData(submittedEquation);
+        setCurrentRelics(submittedRelics);
+        console.log('hello ' + roomId)
+        socket.emit('playerReady', roomId, submittedEquation, deconstructedEquation, submittedRelics);
     }
 
-    function newRound(gameState: string) {
-        if (gameState === 'build'){
-            setGameState('build');
-        }
+    function closePopUp() {
+        setPopUpDetails(null);
     }
+    function closeWarning() {
+        setShowWarning(false);
+    }
+    function redirectToLobby() {
+        socket.emit('end');
+        navigate('/');
+    }
+    function newGame() {
+        socket.emit('newGame', roomId);
+    }
+
+
+
     return (
         <div>
-            {loading ? <div>Loading...</div> :
+            {loading ?
+                <div style={{ paddingTop: '50px' }}>
+                    <div>oops, you've exited the match!</div>
+                    <button onClick={redirectToLobby}>return to lobby</button>
+                </div> :
                 <div>
-                    {gameState === 'build' && <BuildPage id="equationConstruct" content={currentBuild!} inventory={currentInventory} relics={currentRelics} coins={currentCoins} sendDataToGame={handleDataFromBuildPage} roomId={roomId!} />}
-                    {gameState === 'assignment' && <CombatPage equation={equationData} rolls={dice} opponentVariables={opponentVariables}></CombatPage>}
-                    {gameState === 'calculate' && <CalculationPage stats={playerProp} changeGameState={newRound} socketID={socket.id} />}
+                    {popUpDetails && <PopUp onClose={closePopUp} message={popUpDetails} />}
+                    {showWarning && <PreventRefresh onClose={closeWarning} returnToLobby={redirectToLobby} />}
+                    <AnimatePresence>
+                        {gameState === 'build' && <motion.div exit={{ opacity: 0 }}><BuildPage id="equationConstruct" content={currentBuild!} inventory={currentInventory} relics={currentRelics} opEquation={opPrevEquation} opReceipt={opponentIconReceipt} coins={currentCoins}
+                            health={currentHealth} sendDataToGame={handleDataFromBuildPage} roomId={roomId!} progress={currentProgress} level={currentLevel} cap={currentCap} triggerError={setPopUpDetails} /></motion.div>}
+                    </AnimatePresence>
+                    <AnimatePresence>
+                        {gameState === 'assignment' && <CombatPage equation={equationData} rolls={dice} opponentVariables={opponentVariables} receipt={receiptData} buffed={buffedEquation}></CombatPage>}
+                    </AnimatePresence>
+                    <AnimatePresence>
+                        {gameState === 'calculate' && <CalculationPage stats={playerProp} changeGameState={setGameState} receipt={iconReceipt} opReceipt={opponentIconReceipt} socketID={socket.id} />}
+                    </AnimatePresence>
+                    {gameState === 'victory' && <GameOver outcome='Victory!' returnToLobby={redirectToLobby} resetGame={newGame} />}
+                    {gameState === 'defeat' && <GameOver outcome='Defeat!' returnToLobby={redirectToLobby} resetGame={newGame} />}
                 </div>
             }
         </div>
