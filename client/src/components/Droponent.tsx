5e671969e8fac5e8b6a46dc088943583aac2e912
git-richardwu <richardwu720@gmail.com>
2025-03-25 21:54:36 -0400

    Update Droponent.tsx

--- client/src/components/Droponent.tsx
+++ client/src/components/Droponent.tsx
@@ -3,39 +3,47 @@ import { Droppable } from "@hello-pangea/dnd";
 import Tile from "./Tile";
 
 interface ItemProps {
-    itemID: string,
-    name: string,
-    icon: string,
-    description: string,
-    rarity: string,
-    cost: number
+    itemID: string;
+    name: string;
+    icon: string;
+    description: string;
+    rarity: string;
+    cost: number;
 }
+
+type propOrNum = ItemProps[] | number[];
 
 interface DroponentProps {
     dropID: string;
     style?: CSSProperties;
-    list: ItemProps[] | number[];
+    list: propOrNum;
     backgroundText: string;
     prefix: string;
     slotCount: string[];
 }
 
 const Droponent: React.FC<DroponentProps> = ({ dropID, style, list, backgroundText, prefix, slotCount }) => {
+
+    function typeGuard(checkThis: propOrNum): checkThis is ItemProps[] {
+        return typeof checkThis[0] === 'object';
+    }
+
     return (
         <Droppable droppableId={dropID} direction="horizontal">
             {provided => (
                 <div style={{ position: 'relative' }} >
-                    <div style={style} ref={provided.innerRef} {...provided.droppableProps}>
-                        {list.map((entry, index) => {
-                            if (typeof entry === 'object') {
-                                return <Tile unique={entry.itemID} text={entry.icon} index={index} key={entry.itemID} />
-                            }
-                            else {
-                                return <Tile unique={`${prefix}-${entry}`} text={String(entry)} index={index} key={`${prefix}-${entry}`} />
-                            } //remove?
+                    {typeGuard(list) ? <div style={style} ref={provided.innerRef} {...provided.droppableProps}>
+                        {list.map((entry: ItemProps, index: number) => {
+                            return <Tile unique={entry.itemID} text={entry.icon} desc={entry.description} highlight={entry.rarity} index={index} key={entry.itemID} />
                         })}
                         {provided.placeholder}
-                    </div>
+                    </div> : <div style={style} ref={provided.innerRef} {...provided.droppableProps}>
+                        {list.map((entry: number, index: number) => {
+                            return <Tile unique={`${prefix}-${entry}-${index}`} text={String(entry)} highlight={"solid white"} index={index} key={`${prefix}-${entry}-${index}`} desc={""} />
+                        })}
+                        {provided.placeholder}
+                    </div>}
+
                     <div className="position" >
                         {slotCount.map((item, index) => (
                             <div className="square" key={`${item}-${index}`}>{item}</div>
@@ -43,7 +51,6 @@ const Droponent: React.FC<DroponentProps> = ({ dropID, style, list, backgroundTe
                     </div>
                     <div className="bg-text">{backgroundText}</div>
                 </div>
-
             )}
         </Droppable>
     )
