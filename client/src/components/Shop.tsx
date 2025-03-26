321f0d65445a19ad1c4e801ccd74b173908c412f
git-richardwu <richardwu720@gmail.com>
2025-03-25 21:48:48 -0400

    Update Shop.tsx

--- client/src/components/Shop.tsx
+++ client/src/components/Shop.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, useState } from 'react'
+import React, { CSSProperties, useEffect, useState } from 'react'
 import './index.scss';
 import { useSocket } from '../context';
 import Tippy from "@tippyjs/react";
@@ -6,27 +6,32 @@ import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/scale.css';
 
 interface ItemProps {
-   itemID: string,
-   name: string,
-   icon: string,
-   description: string,
-   rarity: string,
-   cost: number
+   itemID: string;
+   name: string;
+   icon: string;
+   description: string;
+   rarity: string;
+   cost: number;
 }
 
 interface buttonStates {
-   [key: string]: boolean
+   [key: string]: boolean;
 }
 
 interface ShopProps {
    style?: CSSProperties;
    wares: ItemProps[];
-   coinCheck: number,
+   coinCheck: number;
    backgroundText: string;
-   roomId: string,
+   roomId: string;
+   relicCount: number;
 }
 
-const Shop: React.FC<ShopProps> = ({ style, wares, coinCheck, roomId }) => {
+interface lookUp<T> {
+   [key: string]: T;
+}
+
+const Shop: React.FC<ShopProps> = ({ style, wares, coinCheck, roomId, relicCount }) => {
    const { socket } = useSocket();
 
    const [boughtStates, setBoughtStates] = useState<buttonStates>({
@@ -41,23 +46,41 @@ const Shop: React.FC<ShopProps> = ({ style, wares, coinCheck, roomId }) => {
       "b8": false,
    })
 
-   const purchaseElement = (e: ItemProps, idx: number) => {
-      if (coinCheck >= e.cost) {
-         socket.emit('pendingPurchase', roomId, e);
+   useEffect(() => {
+      socket.on('toggleButton', (disableIndex: number) => {
          setBoughtStates((prev) => ({
-            ...prev, [`b-${idx}`]: !prev[`b-${idx}`]
+            ...prev, [`b-${disableIndex}`]: true
          }));
-      }
-      console.log(e)
+      })
+   }, [boughtStates, socket])
+
+   function purchaseElement(e: ItemProps, idx: number) {
+      socket.emit('pendingPurchase', roomId, e, idx);
+   }
+
+   const colors: lookUp<string> = {
+      'common': "#80EF80",
+      'rare': '#1E96FC',
+      'epic': '#C74FE8',
+      'legendary': '#FDB833'
    }
 
 
    return (
       <div style={style}>
          {wares.map((item, index) => (
-            <button className='buyButton' key={"b-" + item.itemID} disabled={boughtStates[`b-${index}`]} onClick={() => purchaseElement(item, index)}>
-               {item.icon}
-            </button>
+            <Tippy key={"T-" + item.itemID + index} content={<span>
+               {item.name}
+               <hr />
+               <span style={{ fontStyle: "oblique", color: colors[item.rarity] }}>{item.description}</span>
+               <br />
+               <span style={{ fontStyle: "italic" }}>🪙: {item.cost}</span>
+            </span>}>
+               <button className='buyButton' key={"B-" + item.itemID} disabled={boughtStates[`b-${index}`]} onClick={() => purchaseElement(item, index)}>
+                  <span className='buttonSurface'>{item.icon}</span>
+               </button>
+            </Tippy>
+
          ))}
          <div className='bg-text'>SHOP</div>
       </div>
