193c6ea19deec38d6bd68c3e8a934e39d47f32f0
git-richardwu <richardwu720@gmail.com>
2025-04-14 16:30:31 -0400

    Update index.js
    Updated CORS origin

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3001", "https://num-brrr.onrender.com"],
         methods: ['GET', 'POST'],
         allowedHeaders: ['Content-Type'],
     }
